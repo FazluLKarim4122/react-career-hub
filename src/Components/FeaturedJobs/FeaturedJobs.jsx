99981c048522276d437d0af719e2e3f5d953602f
FazluLKarim4122 <md.fazlulkarim4122@gmail..com>
2024-03-23 06:00:46 +0600

    cart data added completly

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -4,6 +4,9 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([])
+    // this is not the best way to load show all data
+    //ekhane initially datar poriman hosse 4ta
+    const [dataLength, setDataLength]=useState(4 )
     useEffect(() => {
         fetch('jobs.json')
             .then(res => res.json())
@@ -18,8 +21,15 @@ const FeaturedJobs = () => {
             </div>
             <div className="grid grid-cols-2 gap-6">
                 {
-                    jobs.map((job, idx) => <Job key={idx} job={job}></Job> )
+                    jobs.slice(0,dataLength).map((job, idx) => <Job key={idx} job={job}></Job> )
                 }
+            </div>
+            {/* event handler set korsi setDataLength ke,
+                kivabe data length she setkorbe .jobs er moddhe jotogula data load kora hoise otogula */}
+            <div className={dataLength === jobs.length ? 'hidden': ''}>
+                
+                <button onClick={()=> setDataLength(jobs.length)}
+                className="btn btn-primary ">Show All Jobs</button>
             </div>
         </div>
     );
--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,11 +1,13 @@
-
+import { MdLocationOn } from "react-icons/md";
+import { CiDollar } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 const Job = ({ job }) => {
-    const { logo,job_title, location, company_name, salary, job_description, job_type, remote_or_onsite } = job
+    const {id, logo,job_title, location, company_name, salary, job_description, job_type, remote_or_onsite } = job
     return (
         <div>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src={logo} alt="Shoes" /></figure>
+            <div className="card card-compact w-96  bg-base-100 shadow-xl">
+                <figure><img className="w-[117px] h-[50px]" src={logo} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{job_title}</h2>
                     <p>{company_name}</p>
@@ -13,8 +15,13 @@ const Job = ({ job }) => {
                         <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#9873FF] mr-4">{remote_or_onsite}</button>
                         <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#9873FF]">{job_type}</button>
                     </div>
+                    <div className="mt-4 flex gap-2 ">
+                        <h2 className="flex"><MdLocationOn className="text-2xl mr-2"></MdLocationOn>{location}</h2>
+                        <h2 className="flex"><CiDollar className="text-2xl mr-2"></CiDollar>{salary}</h2>
+                    </div>
                     <div className="card-actions ">
-                        <button className="btn btn-primary">View Details</button>
+                        <Link to={`/job/${id}`}>
+                        <button className="btn btn-primary">View Details</button></Link>
                     </div>
                 </div>
             </div>
